3ae26c76c02481e8afe3240bcbee381e8d90b2ea
tariel <tarieltitirashvili@gmail.com>
2023-09-10 12:34:30 -0700

    refactor DB connection function

--- task_manager/app.js
+++ task_manager/app.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const { tasksRoutes } = require('./routes/tasks');
-require('./connection/db')
+const express = require("express");
+const { tasksRoutes } = require("./routes/tasks");
+const connectionToDB = require("./db/connection");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
-app.use('/api/v1/tasks', tasksRoutes);
+app.use("/api/v1/tasks", tasksRoutes);
 
-app.listen(PORT, () => console.log(`server is listening on ${PORT}...`));
+const start = async () => {
+  try {
+    await connectionToDB(process.env.MONGO_CONNECTION_URI);
+    app.listen(PORT, () => console.log(`server is listening on ${PORT}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
--- task_manager/connection/index.js
+++
@@ -1,3 +0,0 @@
-const CONNECTION_STRING = 'mongodb+srv://tariel_titirashvili:987123654Probook@cluster0.bbus9i4.mongodb.net/?retryWrites=true&w=majority'
-
-module.exports = CONNECTION_STRING
