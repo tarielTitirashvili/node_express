a783527a721b32923a38b1784d426bd53219fe2b
tarielTitirashvili <tarieltitirashvili@gmail.com>
2023-09-13 09:45:02 +0400

    set port depending on ENV variable

--- task_manager/app.js
+++ task_manager/app.js
@@ -6,7 +6,7 @@ const errorHandler = require("./middleware/errorHandler");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.static("./public"));
