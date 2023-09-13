151d7eccf1a4302c4cb82c5ecaffc3fdc8107111
tarielTitirashvili <tarieltitirashvili@gmail.com>
2023-09-13 09:38:18 +0400

    fix error handling

--- task_manager/errors/customError.js
+++ task_manager/errors/customError.js
@@ -1,10 +1,13 @@
 class CustomAPIError extends Error {
   constructor(message, statusCode) {
     super(message);
-    this.statusCode = statusCode;
+    this.status = statusCode;
   };
 };
 
-const createCustomAPIError = (message, statusCode) => new CustomAPIError(message, statusCode);
+const createCustomAPIError = (msg, status) =>{ 
+  // console.log(message, statusCode)
+  return new CustomAPIError(msg, status);
+}
 
 module.exports = { CustomAPIError, createCustomAPIError };
