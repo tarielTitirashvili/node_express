8eb046d93c9a4571e32e662abf31bb950e3bd263
tarielTitirashvili <tarieltitirashvili@gmail.com>
2023-10-09 17:05:19 +0400

    connect to DB

--- online_store/app.js
+++ online_store/app.js
@@ -2,29 +2,32 @@ const express = require('express');
 const errorMiddleware = require('./middleware/error-handler');
 const notFoundMiddleware = require('./middleware/not-found');
 require("dotenv").config();
+const connectDB = require('./db/connect');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 //parse json
-app.use(express.json())
+app.use(express.json());
 
 //error middleware
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 //not found middleware
-app.use(notFoundMiddleware)
+app.use(notFoundMiddleware);
 
 app.get('/', (req, res)=>{
-  res.send(`<h1>store API</h1>`)
-})
+  res.send(`<h1>store API</h1>`);
+});
 
-const start = () => {
+const start = async () => {
   try {
+    // connect to DB
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () => console.log(`server is listening on ${port}...`));
   }catch(e){
-    console.log(e)
-  }
-}
+    console.log(e);
+  };
+};
 
-start()
+start();
