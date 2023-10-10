dcedfcde14339c885e8d43c301a22d9af8694be5
tarielTitirashvili <tarieltitirashvili@gmail.com>
2023-10-10 11:00:49 +0400

    add express-async-errors

--- online_store/app.js
+++ online_store/app.js
@@ -1,5 +1,6 @@
+require('express-async-errors');
 const express = require('express');
-const errorMiddleware = require('./middleware/error-handler');
+// const errorHandlerMiddleware = require('./middleware/error-handler');
 const notFoundMiddleware = require('./middleware/not-found');
 require("dotenv").config();
 const connectDB = require('./db/connect');
@@ -15,16 +16,16 @@ app.use(express.json());
 //routes
 app.use('/api/v1/products', productsRouter);
 
-//error middleware
-app.use(errorMiddleware);
-
 //not found middleware
 app.use(notFoundMiddleware);
+
+//error middleware
+// app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
     // connect to DB
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => console.log(`server is listening on ${port}...`));
   }catch(e){
     console.log(e);
