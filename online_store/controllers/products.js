f8938183c7802cca03dd6faade5f15abe0e98a94
tariel <tarieltitirashvili@gmail.com>
2023-10-12 11:01:49 -0700

    check featured param

--- online_store/controllers/products.js
+++ online_store/controllers/products.js
@@ -1,18 +1,23 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find( req.query);
+  const {name, featured} = req.query;
+  const requestedOBJ = {};
+  if(featured){
+    let featuredStatus = featured === 'true' ? true : featured === 'false' && false;
+    if(featuredStatus)
+      requestedOBJ.featured = featuredStatus;
+  };
+  const products = await Product.find({featured});
   res.status(200).json({products, length: products.length});
 };
 
 const getAllProductsStatic = async (req, res) => {
-  const {name, featured} = req.query;
-  // throw new Error('getAllProducts error')
   const products = await Product.find();
   res.status(200).json({products});
 };
 
 module.exports = {
   getAllProductsStatic,
-  getAllProducts
+  getAllProducts,
 };
