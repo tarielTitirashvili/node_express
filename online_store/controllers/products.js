961ab2257107b5dfd3642e498a54410b5e537004
tariel <tarieltitirashvili@gmail.com>
2023-10-12 11:28:41 -0700

    search by name of product

--- online_store/controllers/products.js
+++ online_store/controllers/products.js
@@ -1,13 +1,22 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-  const {name, featured} = req.query;
+  const {company, featured, name} = req.query;
   const requestedOBJ = {};
+  // check featured
   if(featured === 'true' || featured === 'false'){
     let featuredStatus = featured === 'true' ? true : false;
     if(featuredStatus)
       requestedOBJ.featured = featuredStatus;
   };
+  // check company
+  if(company)
+    requestedOBJ.company = company;
+  // check name
+  if(name)
+    requestedOBJ.name = {$regex: name, $options: 'i'};
+
+  console.log(requestedOBJ);
   const products = await Product.find(requestedOBJ);
   res.status(200).json({products, length: products.length});
 };
