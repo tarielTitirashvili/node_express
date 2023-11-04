9d8aeb6df4edd74b2fda20b832ec7a09ea2d90bd
tariel <tarieltitirashvili@gmail.com>
2023-11-04 10:23:56 -0700

    user schema

--- jobs_API/app.js
+++ jobs_API/app.js
@@ -3,7 +3,7 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
 
 // routes
 const authRoutes = require("./routes/auth");
@@ -31,7 +31,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ jobs_API/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    typeof: 'string',
+    required: [true, 'username must be provided!'],
+    minlength: 2,
+    maxlength: 25,
+  },
+  email: {
+    typeof: 'string',
+    required: [true, 'username must be provided!'],
+    match: [/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, 'please provide valid username!'],
+    unique: 'true'
+  },
+  password: {
+    typeof: 'string',
+    required: [true, 'username must be password!'],
+    minlength: 6,
+    maxlength: 50,
+  },
+});
+
+module.exports = UserSchema;
