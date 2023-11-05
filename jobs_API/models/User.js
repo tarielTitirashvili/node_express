5f7fc2c9631c0870501453c18a78d1e7764ded28
tariel <tarieltitirashvili@gmail.com>
2023-11-04 23:50:24 -0700

    fix user model

--- jobs_API/models/User.js
+++ jobs_API/models/User.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: {
-    typeof: 'string',
+    type: String,
     required: [true, 'username must be provided!'],
     minlength: 2,
     maxlength: 25,
   },
   email: {
-    typeof: 'string',
+    type: String,
     required: [true, 'username must be provided!'],
     match: [/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, 'please provide valid username!'],
-    unique: 'true'
+    unique: true
   },
   password: {
-    typeof: 'string',
+    type: String,
     required: [true, 'username must be password!'],
     minlength: 6,
     maxlength: 50,
