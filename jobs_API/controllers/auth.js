fb4fe73c781c806f9597b44622deab04744279dc
tariel <tarieltitirashvili@gmail.com>
2023-11-05 00:54:25 -0700

    registration logic

--- jobs_API/controllers/auth.js
+++ jobs_API/controllers/auth.js
@@ -1,5 +1,16 @@
-const register = (req, res) => {
-  res.send("user was registered!");
+const Errors = require("../errors");
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
+const register = async (req, res) => {
+  const body = req.body;
+  console.log(req.body);
+  if (!body.name || !body.email || !body.password) {
+    throw new Errors.BadRequestError("please provide valid credentials!");
+  } else {
+    const user = await User.create({...req.body});
+    res.status(StatusCodes.CREATED).json({ msg: "user was registered!", user });
+  }
 };
 
 const login = (req, res) => {
