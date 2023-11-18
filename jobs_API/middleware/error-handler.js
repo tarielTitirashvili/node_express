08ac366941fc3b80f0af98c0b3aa5a4eddc1ec63
tariel <tarieltitirashvili@gmail.com>
2023-11-17 22:41:55 -0800

    generate better error for mongoose duplicate error

--- jobs_API/middleware/error-handler.js
+++ jobs_API/middleware/error-handler.js
@@ -1,6 +1,7 @@
 const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
+
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || "something went wrong please try again later",
@@ -11,8 +12,18 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .json({ msg: customError.message });
   }
   if (err.code) {
+    const mongoError = Object.keys(
+      err.keyValue
+    )
     customError.statusCode = StatusCodes.BAD_REQUEST;
-    customError.message = `user already exists for ${err.keyValue.email}`;
+    customError.message = `duplicate value was entered in ${mongoError.map(
+      (key, index) =>
+        `${key}${
+          index === mongoError.length - 1
+            ? " "
+            : ", "
+        }`
+    )}field`;
   }
   return res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
