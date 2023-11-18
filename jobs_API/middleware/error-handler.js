67d300a257bff8a7ae32de6226502853e82a5058
tariel <tarieltitirashvili@gmail.com>
2023-11-17 22:27:21 -0800

    new error message for duplicate emails

--- jobs_API/middleware/error-handler.js
+++ jobs_API/middleware/error-handler.js
@@ -1,14 +1,22 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    message: err.message || 'something went wrong please try again later'
-  }
+    message: err.message || "something went wrong please try again later",
+  };
   if (err instanceof CustomAPIError) {
-    return res.status(customError.statusCode).json({ msg: customError.message })
+    return res
+      .status(customError.statusCode)
+      .json({ msg: customError.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: customError.message })
-}
+  if (err.code) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = `user already exists for ${err.keyValue.email}`;
+  }
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ msg: customError.message });
+};
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
