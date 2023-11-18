b64da84da8e69417139256794933d01a50e8db86
tariel <tarieltitirashvili@gmail.com>
2023-11-18 07:07:36 -0800

    express-rate-limit for saving from to many requests

--- jobs_API/app.js
+++ jobs_API/app.js
@@ -2,7 +2,24 @@ require("dotenv").config();
 require("express-async-errors");
 const authMiddleWare = require('./middleware/authentication')
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require('express-rate-limit');
+
 const app = express();
+
+app.set('trust proxy', 1)
+
+
+app.use(rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	// standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	// legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+}));
+app.use(helmet());
+app.use(cors());
 
 const connectDB = require("./db/connect");
 
